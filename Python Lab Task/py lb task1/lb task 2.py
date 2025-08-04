00a76d57a8420a7796616db7ceb340b2191e5221
Karthick rajakannathasan <77183053+karthickrajak@users.noreply.github.com>
2025-08-04 11:56:22 +0530

    Add files via upload

---
+++ Python Lab Task/py lb task1/lb task 2.py
@@ -0,0 +1,60 @@
+# print name and qualification
+print("Name:", "Karthickraja Kannathasan")
+print("Qualification:", "B.E Computer Science")
+
+#2. Assign 2 number and apply all arithmetic operations
+
+a=10
+
+b=20
+
+# Arithmetic Operations
+
+print("Addition (a+b):", a+b)
+print("Subraction (a-b):", a-b)
+print("Multiplication (a*b):", a*b)
+print("Division (a/b):", a/b)
+print("Floor Division (a//b):", a//b)
+print("Modulus (a%b):", a%b)
+
+
+
+"""3.Assign 2 numbers a and b and write code to swap using third
+variable."""
+
+a=10
+b=20
+
+print("Before Swapping")
+print("a=",a)
+print("b=",b)
+
+# Using third variable
+print("\n After Swapping")
+temp =a
+a=b
+b=temp
+
+print("a=",a)
+print("b=",b)
+
+
+"""4.Without using third variable and arithmetic operators swap two
+given numbers."""
+
+a=100
+b=200
+
+print("Before Swapping")
+print("a=",a)
+print("b=",b)
+
+#Without Third Variable 
+
+print("\n After Swapping")
+a,b = b,a
+
+print("a=",a)
+print("b=",b)
+
+
