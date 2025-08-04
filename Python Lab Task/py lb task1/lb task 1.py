8b1bd05c7edc39eb2548c8705e2c7bb7f4d9c997
karthickrajak <karthickrajakannathasank@gmail.com>
2025-08-04 11:15:03 +0530

    created lb task 1.py

---
+++ Python Lab Task/py lb task1/lb task 1.py
@@ -0,0 +1,14 @@
+#1. print ("welcome to accord")
+
+print("welcome to accord")
+
+#2. combine more then one statement
+
+print("hello,"+"world!" + "Welcome to Accord")
+
+#3.  add print statement program with end sep parameter
+
+print("Apple","Banana","cherry", sep="|")
+
+print("Loading", end="....")
+print("Done")
